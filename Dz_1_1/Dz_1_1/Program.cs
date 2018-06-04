27f2acc4ed8998a267f9a7b3186fdccf39235bc3
Kharkovskiy IS <his1994@mail.ru>
2018-06-04 13:30:10 +0300

    For home
    For home

---
+++ Dz_1_1/Dz_1_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_1_1
+{
+    class Program
+    {             
+        static void Nomber(int nomber)
+        {
+            char[] arrayNomber = nomber.ToString().ToCharArray();
+            for (int i = 0; i < arrayNomber.Length; i++)
+            { Console.WriteLine(arrayNomber[i]); }
+        }
+
+        static void Main(string[] args)
+        {
+            int nomber = 0;
+            Console.Write("Введите пятизначное число = ");
+            try
+            {
+                nomber = int.Parse(Console.ReadLine());                
+            }
+            catch
+            {
+                Console.WriteLine("---Неверный формат ввода!---");
+            }
+
+            if (nomber / 10000 >= 1)
+            {
+                if (nomber / 100000 < 1)
+                { Nomber(nomber); }
+                else
+                { Console.WriteLine("---Число больше требуемого!---"); }
+            }  
+            else
+            { Console.WriteLine("---Число меньше требуемого!---"); }
+            Console.ReadLine();
+        } 
+    }
+}
---
+++ Dz_1_1/Dz_1_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число в метрах = ");
+            int meter = int.Parse(Console.ReadLine());
+            meter /= 1000;
+            Console.WriteLine("{0} киллометров.", meter);
+            Console.ReadLine();
+        }
+    }
+}
